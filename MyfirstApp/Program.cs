b09f21f84e1b0452b291b14e35d31513a42968e2
Markk <you@example.com>
2024-06-01 11:55:23 +0300

    app

---
+++ MyfirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakenuds tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //output
+//string- sõne
+string userName;
+
+
+userName = Console.ReadLine(); //Input
+Console.WriteLine("Hello" + "," + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
